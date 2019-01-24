03d4cdce0e7e819137c9b9a8c090b47883e7f827
Tobias Dossinger <tobias.dossinger@stud.h-da.de>
2019-01-24 16:23:36 +0100

    fix

--- Arduino-Sketches/Wi-Fi-Sockets/Wi-Fi-Sockets.ino
+++ Arduino-Sketches/Wi-Fi-Sockets/Wi-Fi-Sockets.ino
@@ -1,9 +1,16 @@
-
 // Load Wi-Fi library
 #include <WiFi.h>
 
-uint8_t motorPin = 12;
-uint8_t motorChannel = 1;
+uint8_t motorPin1 = 12;
+uint8_t motorPin2 = 14;
+uint8_t motorPin3 = 27;
+uint8_t motorPin4 = 26;
+
+
+uint8_t motorChannel1 = 1;
+uint8_t motorChannel2 = 2;
+uint8_t motorChannel3 = 3;
+uint8_t motorChannel4 = 4;
 
 
 // Replace with your network credentials
@@ -29,9 +36,16 @@ void setup() {
   Serial.println(WiFi.localIP());
 
     // Attatch motor pin to channel
-  ledcAttachPin(motorPin, motorChannel);
+  ledcAttachPin(motorPin1, motorChannel1);
+  ledcAttachPin(motorPin2, motorChannel2);
+  ledcAttachPin(motorPin3, motorChannel3);
+  ledcAttachPin(motorPin4, motorChannel4);
+  
   // Initialize motor channel with 12 kHz PWM frequency and 8-bit resolution
-  ledcSetup(motorChannel, 12000, 8);
+  ledcSetup(motorChannel1, 12000, 8);
+  ledcSetup(motorChannel2, 12000, 8);
+  ledcSetup(motorChannel3, 12000, 8);
+  ledcSetup(motorChannel4, 12000, 8);
  
   wifiServer.begin();
 }
@@ -44,19 +58,30 @@ void loop(){
  
     while (client.connected()) {
 
-      String message = "";
- 
-      while (client.available()>0) {
-        char c = client.read();
-        message = String(message + c);
-      }
+      int messagePart = 0;
+      String motorID = "";
+      String motorPower = "";
 
-      if(message != "") {
-        Serial.println(message);
-        ledcWrite(motorChannel, message.toInt());
+      while (client.available()>0) {
+
+        char c = client.read();
+        if(c == '-') {
+          messagePart++;
+        } else if(c == 'x') {
+          Serial.print("Motor: ");
+          Serial.print(motorID);
+          Serial.print(" - Power: ");
+          Serial.println(motorPower);
+          ledcWrite(motorID.toInt(), motorPower.toInt());
+          motorID = "";
+          motorPower = "";
+          messagePart = 0;
+        } else if(messagePart == 0) {
+           motorID = motorID + c;
+        } else if(messagePart == 1) {
+          motorPower = motorPower + c;
+        }
       }
-      
- 
       delay(10);
     }
  
